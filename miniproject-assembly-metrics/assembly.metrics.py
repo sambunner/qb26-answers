da0a0ce94c78e4035f2e5cf7e558d9db9f7830e4
Samantha Bunner <sbunner1@jh.edu>
2026-09-10 16:35:51 -0400

    added notes to python script

--- miniproject-assembly-metrics/assembly.metrics.py
+++ miniproject-assembly-metrics/assembly.metrics.py
@@ -3,7 +3,7 @@
 import sys
 import fasta
 
-
+#the first argument as filename and use fasta.Reader
 file = open (sys.argv[1])
 genomes= fasta.FASTAReader(file)
 
@@ -12,10 +12,11 @@ total_length=0
 for indent, sequence in genomes:
         #Count number of loops
     loops +=1
+    #count the length of sequence
     total_length += int(len(sequence))
     
-
+#calculate average length
 average_length = total_length/loops
 
-
+#print result as a single line
 print('number of loops:', loops, 'total_lenght:', total_length, 'average length', average_length )
